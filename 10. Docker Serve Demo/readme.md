d2ac7960e04d5f3bf47069968b07ce8f4d55bce5
vicky seth <vickyseth9933@gmail.com>
2022-11-06 15:21:23 +0530

    added more files

---
+++ 10. Docker Serve Demo/readme.md
@@ -0,0 +1,2 @@
+
+This Image uses serve.js, a node.js module used to serve file directories, and static html pages.
---
+++ 11. Docker Flask Demo/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def response():
+    return "Hello world from Flask container"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
